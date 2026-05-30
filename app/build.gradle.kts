186dada52fd821abc5215b8ac6e9e2ec0ae33be3
최종수 <chlwhdtn03@naver.com>
2026-05-30 19:21:51 +0900

    릴리즈 1.0.9 준비

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,8 +51,8 @@ android {
         applicationId = "com.yourssu.ssutime.v2"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.0.8"
+        versionCode = 11
+        versionName = "1.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField(
