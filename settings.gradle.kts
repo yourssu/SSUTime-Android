3f1b718de563325946ea341efef50f9052610341
최종수 <chlwhdtn03@naver.com>
2026-09-08 23:18:25 +0900

    로컬 종속성 제거

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,7 +17,6 @@ plugins {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        mavenLocal()
         google()
         mavenCentral()
     }
